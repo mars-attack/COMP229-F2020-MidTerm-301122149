31914a63514e1a6f626d624c51d62968bdc72a47
Mari <palmer.m07@gmail.com>
2020-10-28 16:58:30 -0400

    Changed db.URI to point to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  // MonggoDB Atlas
+  "URI": "mongodb+srv://marianne:QUQ8NsXl4GotPVvP@mongodbserver.yioys.mongodb.net/books?retryWrites=true&w=majority"
 };
